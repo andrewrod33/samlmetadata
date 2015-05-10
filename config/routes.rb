ae80c76fd5d315313c07312a254bad3a29e370d3
Andrew Rodriguez <andrew.rodriguez@onelogin.com>
2015-05-10 10:04:07 -0700

    Add Parse Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'parse/home'
+
   root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
